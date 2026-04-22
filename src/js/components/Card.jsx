047f6f7ed6b797393336636ab193679583703959
Lucas Ezequiel Hernandez <lucasezequielhernandez@gmail.com>
2026-04-22 16:17:49 +0000

    First Commit

---
+++ src/js/components/Card.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Card = (props) => {
+    return (
+        <div className="card m-2" style={{ width: "18rem" }}>
+            <img src={props.imgUrl} className="card-img-top" alt="imagen" />
+            <div className="card-body">
+                <h5 className="card-title">{props.title}</h5>
+                <p className="card-text">{props.description}</p>
+                <a href="#" className="btn btn-primary">Ver más</a>
+            </div>
+        </div>
+    );
+};
+
+export default Card;
--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,29 @@
 import React from "react";
+import Card from "./Card";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+    return (
+        <div className="container mt-5">
+            <h1 className="text-center">Mi Landing Page con React</h1>
+            <div className="d-flex justify-content-center flex-wrap">
+                <Card 
+                    imgUrl="https://via.placeholder.com/500x325" 
+                    title="Card Title 1" 
+                    description="Esta es la descripción de mi primera tarjeta creada con props." 
+                />
+                <Card 
+                    imgUrl="https://via.placeholder.com/500x325" 
+                    title="Card Title 2" 
+                    description="Aca pongo información relevante sobre este proyecto." 
+                />
+                <Card 
+                    imgUrl="https://via.placeholder.com/500x325" 
+                    title="Card Title 3" 
+                    description="React hace que esto sea muy fácil de reutilizar." 
+                />
+            </div>
+        </div>
+    );
 };
 
 export default Home;
